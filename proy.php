3605a42dad0e8005d21e998200d8115e86373edc
Gers159 <54990546+Gers159@users.noreply.github.com>
2019-09-09 12:53:04 -0500

    qwe

--- proy.php
+++ proy.php
@@ -1,16 +1,39 @@
 <html> 
 <head> 
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <title>Formulario.</title> 
 </head> 
 
 <body> 
+  
+<center><h1>Calculadora</h1></center>
+<br>
+<center>
+<div class="container">
+<div class="col-3">
+<div class="card">
+    <div class="card-body">
+    <div class="card-auto">
+        
+    <form method="POST" action="sumar.php">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Valor1</label>
+            <input type="text" name="T1" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingrese Valor">
+            
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Valor2</label>
+            <input type="text" name="T2" class="form-control" id="exampleInputPassword1" placeholder="Ingrese Valor">
+        </div>
 
-<form method="POST" action="sumar.php">
-    <p>Valor 1: <input type="text" name="T1" size="20"></p>
-    <p>Valor 2: <input type="text" name="T2" size="20"></p>
-    <p>Valor 3: <input type="text" name="T3" size="20"></p>
-    <p><input type="submit" value="Sumar" name="B1"></p>
-</form>
-
+        <center><button type="submit" class="btn btn-primary"name="B1">Submit</button><center>
+    </form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</center>
 </body>
 </html>
--- sumar.php
+++ sumar.php
@@ -1,35 +1,25 @@
 <html> 
+
 <head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <title>Sumar.</title> 
 </head> 
+
 <body> 
 
 <?php
 
 $valor1 = $_POST['T1'];
 $valor2 = $_POST['T2'];
-$valor3 = $_POST['T3'];
 
-$suma = $valor1 + $valor2 + $valor3;
 
-echo "$valor1 + $valor2 + $valor3 = $suma";
+$suma = $valor1 + $valor2 ;
+
+echo "$valor1 + $valor2  = $suma";
+
+
 
 ?>
 
 </body>
 </html>
 
-<form method="POST" action="sumar.php">
-  <div class="form-group">
-    <label for="exampleInputValor1">Valor1</label>
-    <input type="valor1" class="form-control" id="exampleInputValor1" aria-describedby="emailHelp" placeholder="que miras">
-  </div>
-
-  <div class="form-group">
-    <label for="exampleInputValor1">Valor2</label>
-    <input type="valor2" class="form-control" id="exampleInputValor2" placeholder="Pon tu numero ">
-  </div>
-
-  <button type="submit" value="Sumar" name="B1">Submit</button>
-</form>
